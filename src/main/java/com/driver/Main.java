9732a92fda9eb1585665153597b42112c7d1b7b1
Vijaysirvi11 <vkc66183@gmail.com>
2024-07-14 20:30:32 +0530

    pushing code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+
+        public static int product(int x, int y){
+            return x*y;
+        }
+        public static int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Product P = new Product();
+        P.product(1, 2);
+        P.product(2, 3, 4);
+        P.product(3.5, 4.7);
+
+
+    }
 
 }
